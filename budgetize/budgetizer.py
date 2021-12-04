eb27c5341b9a8b8e1f950215d86069abf8294129
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-12-03 22:34:07 -0600

    Move burndown table into a separate module

--- budgetize/budgetizer.py
+++ budgetize/budgetizer.py
@@ -10,9 +10,7 @@
 # LAST EDITED:      12/03/2021
 ###
 
-from .account import Account
-from .cellname import getCellNameFromCoordinates
-from .cellrange import CellMatrix
+from .burndown import BurndownCalculator
 from .sheet import SheetTable
 
 class Budgetizer:
@@ -30,44 +28,9 @@ class Budgetizer:
                 'com.sun.star.sheet.Spreadsheet')
             self.sheetDoc.getSheets().insertByName(
                 burndownTableSheetName, burndownTableSheet)
-
-        # Put togther dictionary of accounts
-        startDate = beginningBalances.getHeaders()[1]
-        accounts = list(map(
-            lambda a: Account(a['Account'].String, a[startDate].Value),
-            beginningBalances))
-        accountNames = map(lambda a: a.getName(), accounts)
-        accounts = dict(zip(accountNames, accounts))
-
-        # Write out Date, Description, & balance for each account for each
-        # transation in the nonRecurringForm
-        numberOfEntries = nonRecurringForm.getCount() + 1
-        headers = ['Date', 'Description', *accounts.keys()]
-        bottomCorner = getCellNameFromCoordinates(
-            len(headers) - 1, numberOfEntries)
-        burndownTable = CellMatrix(f'A1:{bottomCorner}', burndownTableSheet)
-        outputIter = iter(burndownTable)
-        headerRowIter = iter(next(outputIter))
-        for header in headers:
-            next(headerRowIter).String = header
-        endDate = beginningBalances.getHeaders()[2]
-        initialBalance = next(outputIter)
-        initialBalanceIter = iter(initialBalance)
-        next(initialBalanceIter).String = startDate
-        next(initialBalanceIter).String = 'Starting Balance'
-        for account in accounts:
-            next(initialBalanceIter).Value = accounts[account].getBalance()
-        previousEntry = initialBalance
-        for transaction in nonRecurringForm:
-            entry = iter(next(outputIter))
-            accountIndex = list(accounts.keys()).index(
-                transaction['Account'].String)
-            account = accounts[transaction['Account'].String]
-            account.updateBalance(transaction['Amount'].Value)
-            next(entry).String = transaction['Date'].String
-            next(entry).String = transaction['Description'].String
-            for account in accounts:
-                next(entry).Value = accounts[account].getBalance()
+        burndownCalculator = BurndownCalculator(
+            nonRecurringForm, beginningBalances, burndownTableSheet)
+        burndownCalculator.run()
 
     def budgetize(self):
         nonRecurringForm = SheetTable(
---
+++ budgetize/burndown.py
@@ -0,0 +1,83 @@
+###############################################################################
+# NAME:             burndown.py
+#
+# AUTHOR:           Ethan D. Twardy <ethan.twardy@gmail.com>
+#
+# DESCRIPTION:      Burndown chart/table logic
+#
+# CREATED:          12/03/2021
+#
+# LAST EDITED:      12/03/2021
+###
+
+from .account import Account
+from .cellname import getCellNameFromCoordinates
+from .cellrange import CellMatrix
+
+class BurndownCalculator:
+    def __init__(
+            self, nonRecurringForm, beginningBalances, burndownTableSheet):
+        self.nonRecurringForm = nonRecurringForm
+        self.beginningBalances = beginningBalances
+        self.burndownTableSheet = burndownTableSheet
+
+    @staticmethod
+    def getAccounts(beginningBalances, startDate):
+        """Assemble dictionary of accounts"""
+        accounts = list(map(
+            lambda a: Account(a['Account'].String, a[startDate].Value),
+            beginningBalances))
+        accountNames = map(lambda a: a.getName(), accounts)
+        return dict(zip(accountNames, accounts))
+
+    @staticmethod
+    def writeHeaders(outputIter, headers):
+        """Write out Date, Description, & balance for each account for each
+        transation in the nonRecurringForm"""
+        headerRowIter = iter(next(outputIter))
+        for header in headers:
+            next(headerRowIter).String = header
+        return outputIter
+
+    @staticmethod
+    def writeInitialBalances(outputIter, startDate, accounts):
+        initialBalance = next(outputIter)
+        initialBalanceIter = iter(initialBalance)
+        next(initialBalanceIter).String = startDate
+        next(initialBalanceIter).String = 'Starting Balance'
+        for account in accounts:
+            next(initialBalanceIter).Value = accounts[account].getBalance()
+        return outputIter
+
+    @staticmethod
+    def writeBurndownTable(nonRecurringForm, outputIter, accounts):
+        """Write transactions to burndown table"""
+        for transaction in nonRecurringForm:
+            entry = iter(next(outputIter))
+            account = accounts[transaction['Account'].String]
+            account.updateBalance(transaction['Amount'].Value)
+            next(entry).String = transaction['Date'].String
+            next(entry).String = transaction['Description'].String
+            for account in accounts:
+                next(entry).Value = accounts[account].getBalance()
+
+    def run(self):
+        startDate = self.beginningBalances.getHeaders()[1]
+        accounts = BurndownCalculator.getAccounts(self.beginningBalances,
+                                                  startDate)
+        headers = ['Date', 'Description', *accounts.keys()]
+        numberOfEntries = self.nonRecurringForm.getCount() + 1
+        bottomCorner = getCellNameFromCoordinates(
+            len(headers) - 1, numberOfEntries)
+        burndownTable = CellMatrix(f'A1:{bottomCorner}',
+                                   self.burndownTableSheet)
+
+        # Write headers and initial balances
+        outputIter = BurndownCalculator.writeInitialBalances(
+            BurndownCalculator.writeHeaders(iter(burndownTable), headers),
+            startDate, accounts)
+        BurndownCalculator.writeBurndownTable(self.nonRecurringForm,
+                                              outputIter, accounts)
+        # endDate = self.beginningBalances.getHeaders()[2]
+
+###############################################################################
